8f7cb85801e49bdf469e27798dda6d22c17941dd
REMR11 <ronaldmejia0211@gmail.com>
2024-06-29 20:07:05 -0600

    feat: Creacion de proyecto base

---
+++ src/test/java/com/SpringAlura/LiterAlura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringAlura.LiterAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
